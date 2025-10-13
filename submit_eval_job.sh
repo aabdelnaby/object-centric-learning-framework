6e1cfa25c7cb3c69e0b97844cef440ee12eff2c1
abdelrahman.abdelnaby@uni-konstanz.de <kn_pop550892@uc3n991.localdomain>
2025-10-13 11:57:57 +0200

    slurm

--- submit_eval_job.sh
+++ submit_eval_job.sh
@@ -6,15 +6,15 @@
 #SBATCH --cpus-per-task=24          
 #SBATCH --gres=gpu:4           
 #SBATCH --time=30             
-#SBATCH --output=slurm_jobs_logs/slurm-%j-movi_c_feat_rec_dino_a100short4gpu.out
-#SBATCH --error=slurm_jobs_logs/slurm-%j-movi_c_feat_rec_dino_a100short4gpu.err
+#SBATCH --output=slurm_jobs_logs/eval/hierarchy/139867steps_2slots_short_sa_gating/slurm-%j-movi_c_feat_rec_dino_a100short4gpu.out
+#SBATCH --error=slurm_jobs_logs/eval/hierarchy/139867steps_2slots_short_sa_gating/slurm-%j-movi_c_feat_rec_dino_a100short4gpu.err
 
 source ~/.bashrc
 conda activate oclf_env   
 
 export DATASET_PREFIX=scripts/datasets/outputs
 poetry run ocl_eval \
-  +train_config_path=/home/kn/kn_kn/kn_pop550892/desktop/object-centric-learning-framework/outputs/projects/bridging/dinosaur/movi_c_feat_rec/2025-09-05_15-11-10/config/config.yaml \
+  +train_config_path=/home/kn/kn_kn/kn_pop550892/desktop/object-centric-learning-framework/outputs/projects/bridging/dinosaur/movi_c_feat_rec/2025-09-15_10-54-00/config/config.yaml \
   +evaluation=projects/bridging/outputs_movi_e.yaml \
-  +checkpoint_path=/home/kn/kn_kn/kn_pop550892/desktop/object-centric-learning-framework/epoch_98-step_90924.ckpt \
+  +checkpoint_path=/home/kn/kn_kn/kn_pop550892/desktop/object-centric-learning-framework/checkpoints/epoch_0-step_7361_hierarchy_2_slots_sa_gating.ckpt \
   +output_dir=.
--- submit_train_job.sh
+++ submit_train_job.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#SBATCH --job-name=movi_c_feat_rec_dino_a100_long_4gpus_hierarchical_slots
+#SBATCH --job-name=train_coco_hierarchical
 #SBATCH --partition=gpu_a100_il
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=24          
 #SBATCH --gres=gpu:4           
-#SBATCH --time=48:00:00          
-#SBATCH --output=slurm_jobs_logs/train_short_movi_hierarchical_slots/slurm-%j-movi_c_feat_rec_dino.out
-#SBATCH --error=slurm_jobs_logs/train_short_movi_hierarchical_slots/slurm-%j-movi_c_feat_rec_dino.err
+#SBATCH --time=48:00:00
+#SBATCH --output=slurm_jobs_logs/train/movi_hierarchical_coco_long/slurm-%j-movi_c_feat_rec_dino_dev.out
+#SBATCH --error=slurm_jobs_logs/train/movi_hierarchical_coco_long/slurm-%j-movi_c_feat_rec_dino_dev.err
 
 source ~/.bashrc
 conda activate oclf_env   
 
 export DATASET_PREFIX=scripts/datasets/outputs
 poetry run ocl_train \
-    +experiment=projects/bridging/dinosaur/movi_c_feat_rec \
+    +experiment=projects/bridging/dinosaur/coco_feat_rec_dino_small16_auto \
     hydra.job.chdir=false 
     
